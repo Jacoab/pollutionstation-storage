fa36987b9e41de25e95581d65f09331264f28d5f
crmoe <60358861+crmoe@users.noreply.github.com>
2020-04-30 13:41:28 -0600

    Delete Data.js

--- Data.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.USER_KEY, { useNewUrlParser: true } );
-mongoose.connection.once('open', function());
-
